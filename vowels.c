3f8cc455474ea6976ba59aad54ddd48bb6e06c59
priyankavijayarahavan <priyankavijayarahavan@gmail.com>
2016-07-11 12:28:38 +0530

    finish

---
+++ vowels.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main()
+{
+char ch;
+printf("enter the alphabet:');
+scanf("%c",&ch);
+switch(ch)
+{
+case 'a':printf("num is vowel");
+break;
+case 'e':printf("num is vowel");
+break;
+case 'i':printf("num is vowel");
+break;
+case 'o':printf("num is vowel");
+break;
+case 'u':printf("num is vowel");
+break;
+case 'A':printf("num is vowel");
+break;
+case 'E':printf("num is vowel");
+break;
+case 'I':printf("num is vowel");
+break;
+case 'O':printf("num is vowel");
+break;
+case 'U':printf("num is vowel");
+break;
+default:printf("num is consonant");
+}
+}
